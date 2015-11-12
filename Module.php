8610fc0a6a2cd4b0a4b0288e42567788b5f72052
Guillaume Badi <gbadi@mailjet.com>
2015-11-12 14:38:16 +0100

    clean

--- Module.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
-* This file is placed here for compatibility with Zendframework's ModuleManager.
-* It allows usage of this module even without composer.
-* The original Module.php lives in 'src' directory in order to respect PSR-0
-*/
-require_once __DIR__ . '/src/Mailjet/Module.php';
