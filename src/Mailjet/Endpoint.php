ecc6b9fcb571aa6d8a166dacded777a76cb89835
Guillaume Badi <gbadi@mailjet.com>
2015-11-16 18:53:20 +0100

    Update Endpoint.php

--- src/Mailjet/Endpoint.php
+++ src/Mailjet/Endpoint.php
@@ -7,8 +7,8 @@
  * @category Mailjet_API
  * @package  Mailjet-apiv3
  * @author   Guillaume Badi <gbadi@mailjet.com>
- * @license  MIT https://licencepath.com
- * @link     http://link.com
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     dev.mailjet.com
  */
 
 namespace Mailjet;
