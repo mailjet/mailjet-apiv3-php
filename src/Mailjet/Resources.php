b4c5431dd399a1bcc04161d6634992406105f0fa
Aridjar <cpaumard@mailjet.com>
2016-12-19 14:29:17 +0100

    remove duplicated commentaries

--- src/Mailjet/Resources.php
+++ src/Mailjet/Resources.php
@@ -1,16 +1,4 @@
 <?php
-/**
- * PHP version 5
- *
- * This is the Mailjet Resources file
- *
- * @category Mailjet_API
- * @package  Mailjet-apiv3
- * @author   Guillaume Badi <gbadi@mailjet.com>
- * @license  MIT https://opensource.org/licenses/MIT
- * @link     dev.mailjet.com
- */
-
 namespace Mailjet;
 
 /**
