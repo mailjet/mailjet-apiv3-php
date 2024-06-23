b0277399acdfa4060c7ce70f55b394ee612914dd
Oleksandr Mykhailenko <mihaskep@gmail.com>
2024-06-23 19:58:28 +0300

    UTF 8  string notation for subject. Support emoji

---
+++ src/Mailjet/Utility/StringUtility.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Mailjet\Utility;
+
+final class StringUtility
+{
+    /**
+     * This method is used to encode a string to UTF-8 notations for using in Subject.
+     * @param string $string
+     * @return string
+     */
+    public static function utfStringNotation(string $string): string
+    {
+        return sprintf('=?UTF-8?B?%s?=', base64_encode($string));
+    }
+}
